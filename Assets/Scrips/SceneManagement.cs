567914b4661fc416023670347521944fb2dbb5c1
Tim <Timsalabim6@gmail.com>
2025-01-19 23:47:27 +0100

    Uwu

--- Assets/Scrips/SceneManagement.cs
+++ Assets/Scrips/SceneManagement.cs
@@ -6,9 +6,20 @@ using UnityEngine.SceneManagement;
 public class SceneManagement : MonoBehaviour
 {
     public string sceneName;
-    
+    public int TransisionNumber;
+    public bool isTransisionScene = false;
+
     public void LoadNextScene()
     {
-        SceneManager.LoadScene(sceneName);
+        if (isTransisionScene)
+        {
+            PlayerPrefs.SetInt("SceneIndex", TransisionNumber);
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
     }
 }
